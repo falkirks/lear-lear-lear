a8b756ff005497035c2b4b6e72eddc0e507fb19b
Falkirks <me@falkirks.com>
2016-04-02 13:00:10 -0700

    cool beans

--- js/main.js
+++ js/main.js
@@ -28,8 +28,8 @@ animate();
         peasentCount = 1000;
         peasents = new THREE.Geometry();
         peasentMaterial = new THREE.PointsMaterial({
-            color: 0xFFFFFF,
-            size: 5
+            size: 10,
+            vertexColors: true
         });
 
 
@@ -58,39 +58,6 @@ animate();
 // add it to the scene
         scene.add(peasentSystem);
 
-
-        kingCount = 1;
-        kings = new THREE.Geometry();
-        kingMaterial = new THREE.PointsMaterial({
-            color: 0xB99B30,
-            size: 20
-        });
-
-
-// now create the individual particles
-        for (var k = 0; k < kingCount; k++) {
-
-            // create a particle with random
-            // position values, -250 -> 250
-            var kX = Math.random() * 800 - 400,
-                kY = Math.random() * 800 - 400,
-                kZ = 0,
-                king = new THREE.Vector3(kX, kY, kZ);
-
-            king.velocity = new THREE.Vector3(Math.random()*3, -Math.random()*3, 0);
-
-            // add it to the geometry
-            kings.vertices.push(king);
-        }
-
-        kingSystem = new THREE.Points(
-            kings,
-            kingMaterial);
-        kingSystem.sortParticles = true;
-
-// add it to the scene
-        scene.add(kingSystem);
-
         renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
 
@@ -102,43 +69,6 @@ animate();
 
         requestAnimationFrame( animate );
 
-        for (var i = 0; i < kings.vertices.length; i++) {
-
-            // get the particle
-            var particle = kings.vertices[i];
-            if(i == 0){
-                console.log(particle);
-            }
-
-            // check if we need to reset
-            if(particle.y < -400 || particle.y > 400){
-                particle.velocity.y *= -1;
-            }
-
-            if(particle.x < -400 || particle.x > 400){
-                particle.velocity.x *= -1;
-            }
-
-            if(particle.z < -400 || particle.z > 400){
-                particle.velocity.z *= -1;
-            }
-
-            // update the velocity with
-            // a splat of randomni
-            //particle.velocity.y -= Math.random() * 0.1;
-
-            // and the position
-            particle.x += particle.velocity.x;
-            particle.y += particle.velocity.y;
-            particle.z += particle.velocity.z;
-        }
-
-        // flag to the particle system
-        // that we've changed its vertices.
-        kingSystem.geometry.verticesNeedUpdate = true;
-
-
-        // Now let's update the peasents
 
         for(i = 0; i < peasents.vertices.length; i++){
             particle = peasents.vertices[i];
@@ -146,7 +76,6 @@ animate();
                 console.log(particle);
             }
 
-            var king = getClosestKing(peasents.vertices[i]);
             var bounced = false;
 
             if(particle.y < -300 || particle.y > 300){
@@ -164,48 +93,36 @@ animate();
                 particle.velocity.z *= -1;
                 bounced = true;
             }
-            /*if(!bounced) {
+            if(!bounced) {
                 for (var j = 0; j < peasents.vertices.length; j++) {
                     var dist = particle.distanceTo(peasents.vertices[j]);
-                    if (dist <= 1) {
+                    if (dist <= 10) {
                         bounced = true;
+                        var avg = new THREE.Vector3();
+                        avg.addVectors(particle.velocity, peasents.vertices[j].velocity);
+                        particle.velocity.subVectors(avg, particle.velocity);
+                        peasents.vertices[j].velocity.subVectors(avg, peasents.vertices[j].velocity);
                         //peasents.vertices[j].velocity.negate();
                     }
-                }
-            }*/
-            if(!bounced) {
-                if(particle.x > king.x ^ particle.velocity.x < 0){
-                    particle.velocity.x *= -1;
-                }
-                if(particle.y > king.y ^ particle.velocity.y < 0){
-                    particle.velocity.y *= -1;
-                }
-                if(particle.z > king.z ^ particle.velocity.z < 0){
-                    particle.velocity.z *= -1;
                 }
             }
 
             particle.x += particle.velocity.x;
             particle.y += particle.velocity.y;
             particle.z += particle.velocity.z;
+            var c = 40 * Math.abs(noise.perlin2(particle.x / 1000, particle.y / 1000));
+
+            peasentSystem.geometry.colors[i] = new THREE.Color(c, c, c);
+            if(i == 0) {
+                console.log(peasentSystem.geometry.colors[i]);
+            }
         }
+        peasentSystem.geometry.colorsNeedUpdate = true;
         peasentSystem.geometry.verticesNeedUpdate = true;
 
 
         renderer.render( scene, camera );
 
-    }
-    function getClosestKing(peasent){
-        var lowDist = 999999999999;
-        var king = null;
-        for (var i = 0; i < kings.vertices.length; i++) {
-            var dist = peasent.distanceTo(kings.vertices[i]);
-            if(dist < lowDist){
-                lowDist = dist;
-                king = kings.vertices[i];
-            }
-        }
-        return king;
     }
 
 });
